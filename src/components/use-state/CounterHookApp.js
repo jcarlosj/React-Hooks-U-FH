f19747d9ded7c807f7928ab3d6b97aa52705dcbc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-24 17:42:09 -0500

    [+] useCounter: Define funcionalidad del hook

--- src/components/use-state/CounterHookApp.js
+++ src/components/use-state/CounterHookApp.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
+/** Custom Hooks */
+import { useCounter } from '../../hooks/useCounter';
+
 import './counter-app.css';
 
 export const CounterHookApp = () => {
 
+    const { state:counter, increment, decrement } = useCounter( 9 );
+
     return (
         <section className="container mt-5">
-            <h1>Counter: { 0 }</h1>
+            <h1>Counter: { counter }</h1>
             <p>Usando un Hook personalizado</p>
             <hr />
             <button 
                 className="btn btn-primary m-1"
-                onClick={ () => {
-                    
-                } }
+                onClick={ increment }
             >+1</button>   
             <button 
                 className="btn btn-primary m-1"
-                onClick={ () => {
-                    
-                } }
+                onClick={ decrement }
             >-1</button>  
         </section>
     )
