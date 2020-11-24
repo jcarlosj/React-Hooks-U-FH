b39baf5a5c00bd92a7f3a89a88d306cc9ddede5c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-24 12:02:40 -0500

    [+] CounterApp: Define Object State

--- src/components/use-state/CounterApp.js
+++ src/components/use-state/CounterApp.js
@@ -2,18 +2,27 @@ import React, { useState } from 'react';
 
 import './counter-app.css';
 
-export const CounterApp = ({ initialValue = 10 }) => {
+export const CounterApp = () => {
 
-    const [ counter, setCounter ] = useState( initialValue );
+    const 
+        [ counter, setCounter ] = useState({
+            counterOne: 10,
+            counterTwo: 20
+        }),
+        { counterOne, counterTwo } = counter;
 
     return (
         <section className="container mt-5">
-            <h1>Counter { counter }</h1>
+            <h1>Counter 1: { counterOne }</h1>
+            <h1>Counter 2: { counterTwo }</h1>
             <hr />
             <button 
                 className="btn btn-primary"
                 onClick={ () => {
-                    setCounter( counter + 1 );
+                    setCounter({
+                        ...counter,
+                        counterOne: counterOne + 1
+                    });
                 } }
             >+1</button>   
         </section>
