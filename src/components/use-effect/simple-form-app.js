4428957341d4d543a8ebe951d6dbb4f7c4f44661
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-25 11:22:37 -0500

    [+] SimpleFormApp: Define State & handleInputChange

--- src/components/use-effect/simple-form-app.js
+++ src/components/use-effect/simple-form-app.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './use-effects.css';
 
 export const SimpleFormApp = () => {
+
+    const 
+        [ formState, setFormState] = useState( {
+            name: '',
+            email: ''
+        } ),
+        { name, email } = formState;
+
+    const handleInputChange = event => {
+        console.log( event.target.name, event.target.value );
+    }
+
     return (
         <section className="container mt-5">
             <h1>useEffect</h1> 
             <hr />  
+
+            <div className="form-group">
+                <input 
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Tu nombre"
+                    autoComplete="off"
+                    value={ name }
+                    onChange={ handleInputChange }  
+                />
+            </div>
         </section>
     )
 }
