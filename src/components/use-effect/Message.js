67c73bf12d1bfc690e98901b70f07d7c168bfebf
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-25 15:33:15 -0500

    [+] Message: Define montaje y desmontaje del componente (Ejemplo)

--- src/components/use-effect/Message.js
+++ src/components/use-effect/Message.js
@@ -1,17 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
 
+    const 
+        [ coords, setCoords ] = useState({
+            x: 0, y: 0
+        }),
+        { x, y } = coords;
+
     useEffect(() => {
         
-        console .log( 'Componente Message montado' );
+        console.log( 'Componente Message montado' );
+
+        const mouseMove = event => {   //  Definimos la funcionalidad
+            const coords = {
+                x: event.x,
+                y: event.y
+            }
+            // console.log( coords );
+            setCoords( coords );     
+        }
+
+        window.addEventListener( 'mousemove', mouseMove );      //  Monta la funcionalidad al listener
 
         return () => {
-            console .log( 'Componente Message desmontado' );
+            console.log( 'Componente Message desmontado' );
+            window.removeEventListener( 'mousemove', mouseMove );   //  Desmonta la funcionalidad del listener 
         }
     }, [])
     
     return (
-        <h3>Eres genial!</h3>
+        <>
+            <h3>Eres genial!</h3>
+            <p>x: { x }, y: { y }</p>
+        </>
     )
 }
