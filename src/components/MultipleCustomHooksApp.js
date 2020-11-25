7ecd846aa5ea709159097d9866b8b9ae60e0e93e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-25 17:06:00 -0500

    [+] MultipleCustomHooksApp: Despliega data del Hook useFetch implementado

--- src/components/MultipleCustomHooksApp.js
+++ src/components/MultipleCustomHooksApp.js
@@ -6,14 +6,26 @@ import { useFetch } from '../hooks/useFetch';
 /** Functional Component */
 export const MultipleCustomHooksApp = () => {
 
-    const state = useFetch( `https://www.breakingbadapi.com/api/quotes/1` );
+    const 
+        { loading, data } = useFetch( `https://www.breakingbadapi.com/api/quotes/1` ),
+        { author, quote } = !!data && data[ 0 ];    //  !!null -> Convertir un null en false
 
-    console.log( state );
+    console.log( quote, '(', author, ')' );
 
     return (
         <section className="container mt-5">
-            <h1>Custom Hooks</h1>  
+            <h1>Breaking Bad Quotes</h1>  
+            <p>Custom Hooks</p>  
             <hr /> 
+    
+            {   loading  
+                    ?   <p className="alert alert-info text-center">Loading...</p> 
+                    :   <blockquote className="blockquote text-right">
+                            <p className="mb-0">{ quote }</p>
+                            <footer className="blockquote-footer">{ author }</footer>
+                        </blockquote>
+            }
+            
         </section>
     )
 }
