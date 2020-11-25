5a55d9049e2cfcbb52bff62a6b769b1bcd8641c1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-25 16:27:54 -0500

    [.] MultipleCustomHooksApp: Define

---
+++ src/components/MultipleCustomHooksApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const MultipleCustomHooksApp = () => {
+    return (
+        <section className="container mt-5">
+            <h1>Custom Hooks</h1>  
+            <hr /> 
+        </section>
+    )
+}
