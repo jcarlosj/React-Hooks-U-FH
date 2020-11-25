febc87890c0d59faad495c0e19ec433379a95309
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-25 16:14:37 -0500

    [.] SimpleFormHookApp: Define handleSubmit

--- src/components/use-effect/SimpleFormHookApp.js
+++ src/components/use-effect/SimpleFormHookApp.js
@@ -20,46 +20,64 @@ export const SimpleFormHookApp = () => {
         console.log( 'Email cambió' );
     }, [ email ]);
 
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log( formValues );
+
+    }
+
     return (
         <section className="container mt-5">
             <h1>useEffect</h1> 
             <p>Usando un Hook personalizado</p>
             <hr />  
 
-            <div className="form-group">
-                <input 
-                    type="text"
-                    name="name"
-                    className="form-control"
-                    placeholder="Tu nombre"
-                    autoComplete="off"
-                    value={ name }
-                    onChange={ handleInputChange }  
-                />
-            </div>
+            <form
+                onSubmit={ handleSubmit }
+            >
+                <div className="form-group">
+                    <input 
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        placeholder="Tu nombre"
+                        autoComplete="off"
+                        value={ name }
+                        onChange={ handleInputChange }  
+                    />
+                </div>
 
-            <div className="form-group">
-                <input 
-                    type="email"
-                    name="email"
-                    className="form-control"
-                    placeholder="Tu email"
-                    autoComplete="off"
-                    value={ email }
-                    onChange={ handleInputChange }  
-                />
-            </div>
+                <div className="form-group">
+                    <input 
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Tu email"
+                        autoComplete="off"
+                        value={ email }
+                        onChange={ handleInputChange }  
+                    />
+                </div>
 
-            <div className="form-group">
-                <input 
-                    type="password"
-                    name="password"
-                    className="form-control"
-                    placeholder="******"
-                    value={ password }
-                    onChange={ handleInputChange }  
-                />
-            </div>
+                <div className="form-group">
+                    <input 
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        placeholder="******"
+                        value={ password }
+                        onChange={ handleInputChange }  
+                    />
+                </div>
+
+                <div className="form-group">
+                    <button 
+                        type="submit"
+                        className="btn btn-primary"
+                    >Guardar</button>
+                </div>
+            
+            </form>
 
         </section>
     )
