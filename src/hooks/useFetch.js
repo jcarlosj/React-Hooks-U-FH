e24f71460b5ebef75b28a4feb1fbec07a8240aec
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-26 16:09:50 -0500

    [+] RealExampleRef: Implementa SOLUCION para prevenir la actualizacion de estado de un componente desmontado

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,26 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 export const useFetch = ( url ) => {
 
     //  TO DO: Validar URL
 
-    const [ state, setState ] = useState({
-        data: null,
-        loading: true,
-        error: null
-    });
+    const
+        isMounted = useRef( true ),             //  El Componente esta montado cuando se renderiza por primera vez 
+        [ state, setState ] = useState({
+            data: null,
+            loading: true,
+            error: null
+        });
 
-    useEffect(() => {
+    useEffect( () => {
+
+        return () => {
+            /** Acciones cuando el componente se desmonte */
+            isMounted.current = false;          //  Mantenemos la referencia del punto de montaje del componente
+        }
+    }, [] );    //  Solo se hará cuando el componente se cargue por primera vez
+
+    useEffect( () => {
 
         /** Establece el State para permitir la carga del Loading entre frases */
         setState({
@@ -25,11 +35,21 @@ export const useFetch = ( url ) => {
 
                 //  TO DO: Evaluar que viene la data
                 setTimeout( () => {
-                    setState({
-                        data,
-                        loading: false,
-                        error: null
-                    });
+
+                    /** Controlamos la peticion de acuerdo al estado de montaje del componente */
+                    if( isMounted.current ) {
+
+                        setState({
+                            data,
+                            loading: false,
+                            error: null
+                        });
+
+                    }
+                    else {
+                        console.log( `Se acaba de prevenir la actualización de estado de un componente desmontado` );
+                    }
+
                 }, 4000 );
 
             });
