27ad6ab67bd7beb4af9ec812cdda2897c7b4d84a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-26 14:41:36 -0500

    [+] FocusScreenApp: Obtener el foco de un campo de forma tradicional

--- src/components/use-ref/FocusScreenApp.js
+++ src/components/use-ref/FocusScreenApp.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export const FocusScreenApp = () => {
+
+    const handleClick = () => {
+        document.querySelector( 'input' ).select();     //  document.querySelector( 'input' ).focus();
+    }
+
     return (
         <section className="container mt-5">
             <h1>Focus Screen</h1>  
@@ -14,7 +19,10 @@ export const FocusScreenApp = () => {
             </div>
 
             <div className="form-group">
-                <button className="btn btn-outline-primary">Focus</button>
+                <button 
+                    className="btn btn-outline-primary"
+                    onClick={ handleClick }
+                >Focus</button>
             </div>
         </section>
     )
