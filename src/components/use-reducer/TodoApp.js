6a45d8b3cf7f902a975165b1fc9dadcb6eb2a577
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-30 10:31:30 -0500

    [.] todoReducer: Implementa useReducer. Inicializa el estado del Reducer. Muestra Data en la Vista

--- src/components/use-reducer/TodoApp.js
+++ src/components/use-reducer/TodoApp.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+/** Reducer */
+import { todoReducer } from './todoReducer';
 
 import './todo-app.css';
 
+
+/** Estado Inicial */
+const initialState = [{
+    id: new Date().getTime(),
+    description: 'Aprender Angular',
+    done: false
+}];
+
 /** Functional Component */
 export const TodoApp = () => {
+
+    const [ todos ] = useReducer( todoReducer, initialState );
+
+    console.log( todos );
+
     return (
         <section className="container mt-5">
-            <h1>To Do</h1>  
+            <h1>To Do ( { todos.length } ) </h1>  
             <p>Usando el Hook useReducer</p>  
             <hr /> 
 
-            <ul>
-                <li>Aprender Angular</li>
-                <li>Aprender Vue</li>
-                <li>Aprender Deno</li>
+            <ul class="list-group list-group-flush">
+               {    todos.map( todo => (
+                        <li 
+                            key={ todo.id }
+                            className="list-group-item"
+                        >{ todo.description }</li>
+                    ))
+               }
             </ul>
 
         </section>
---
+++ src/components/use-reducer/todoReducer.js
@@ -0,0 +1,13 @@
+/** Define funcion pura como reducer (controlar acciones) */
+export const todoReducer = ( state = [], action ) => {
+
+    switch ( action.type ) {
+        // case 'ADD':
+            
+        //     break;
+    
+        default:
+            return state;
+    }
+
+}
