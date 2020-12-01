b47cb4ea6d7a6c404cbf865a56a57636987b8e8b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-01 15:17:09 -0500

    [+] NavBar: Implementa Componente NavBar de Bootstrap

--- src/components/use-context/NavBar.js
+++ src/components/use-context/NavBar.js
@@ -4,12 +4,21 @@ import { Link } from 'react-router-dom';
 /** Functional Component */
 export const NavBar = () => {
     return (
-        <nav className="container mt-5">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+            <Link className="navbar-brand" to="/">useContext</Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Login</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
