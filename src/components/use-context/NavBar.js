7562db6828e23c5a64acd97b9c1a5abbd593a196
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-01 15:24:28 -0500

    [+] NavBar: Implementa enlaces activos usando Componente NavLink de React Router

--- src/components/use-context/NavBar.js
+++ src/components/use-context/NavBar.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 /** Functional Component */
 export const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">useContext</Link>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/">Home</Link>
+                    <li className="nav-item">
+                        <NavLink exact activeClassName="active" className="nav-link" to="/home">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
+                        <NavLink exact activeClassName="active" className="nav-link" to="/about">About</NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/login">Login</Link>
+                        <NavLink exact activeClassName="active" className="nav-link" to="/login">Login</NavLink>
                     </li>
                 </ul>
             </div>
