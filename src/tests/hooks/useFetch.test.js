f1707e430aefcf8556f95274e1cf0ad0c3eadc7b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-02 15:44:46 -0500

    [+] useFetch.test: (Enzime): Define 'debe retornar objecto con data obtenida de la peticion'

--- src/tests/hooks/useFetch.test.js
+++ src/tests/hooks/useFetch.test.js
@@ -17,7 +17,23 @@ describe( 'useFetch Custom Hook', () => {
         expect( data ).toBe( null );
         expect( loading ).toBe( true );
         expect( error ).toBe( null );
-        
+
+    } );
+
+    test( 'debe retornar objecto con data obtenida de la peticion', async () => {
+
+        const 
+            url = `https://www.breakingbadapi.com/api/quotes/1`,
+            { result, waitForNextUpdate } = renderHook( () => useFetch( url ) );       //  Peticion Sincrona
+            
+        await waitForNextUpdate();      //  Espera que se resuelva la peticion
+
+        const { data, loading, error } = result.current;              //  Valores iniciales
+
+        console.log( result.current );
+        expect( data.length ).toBe( 1 );
+        expect( loading ).toBe( false );
+        expect( error ).toBe( null );
 
     } );
 
