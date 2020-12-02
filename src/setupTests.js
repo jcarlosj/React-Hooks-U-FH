c2c37c279ec3e53cf87b87a11f7010afc1d41fbd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-02 10:09:29 -0500

    [+] Enzyme: Configuracion Global de Testing de Enzyme To JSON para el Proyecto

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
 
 /** Configuracion Global para Enzyme */
 import { configure } from 'enzyme';
+import { createSerializer  } from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer( createSerializer({ mode: 'deep' }) );
