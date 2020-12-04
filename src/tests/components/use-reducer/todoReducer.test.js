d43f91d62b08dbbe4bdc8eefe4bb8244d664484a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-04 09:32:47 -0500

    [/] todoReducer.test: Define 'Fixtures' (Datos fijos estaticos de prueba)

--- src/tests/components/use-reducer/todoReducer.test.js
+++ src/tests/components/use-reducer/todoReducer.test.js
@@ -5,13 +5,8 @@ import { shallow } from 'enzyme';
 /** Reducer */
 import { todoReducer } from '../../../components/use-reducer/todoReducer';
 
-const 
-    demoDataTodos = [
-        { id: 1,  description: 'Aprender Vue', done: 'false' },
-        { id: 2,  description: 'Aprender Angular', done: 'false' },
-        { id: 3,  description: 'Aprender Jest', done: 'false' }
-    ],
-    action = {};
+/** Fixtures (Datos fijos de prueba) */
+import { demoDataTodos, action } from '../../fixtures/demoTodos';
 
 describe( 'todoReducer Reducer', () => {
 
