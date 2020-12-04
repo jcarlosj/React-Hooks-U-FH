7fa2237352045c90452a8e9cc85e3ba8f30cbf9e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-04 16:43:17 -0500

    [+] TodoForm.test: Define 'debe invocar handleAddTodo'

--- src/tests/components/use-reducer/TodoForm.test.js
+++ src/tests/components/use-reducer/TodoForm.test.js
@@ -29,4 +29,31 @@ describe( '<TodoForm />', () => {
 
     } );
 
+    test( 'debe invocar handleAddTodo', () => {
+
+        const value = 'Aprender Flutter';
+        
+        /** Simula el cambio de valor de la descripcion (campo input de nombre = description) */
+        wrapper.find( 'input' ).simulate( 'change', {
+            target: {
+                value,
+                name: 'description'
+            }
+        } );
+
+        const $formSubmit = wrapper.find( 'form' ).prop( 'onSubmit' );      //  Obtengo la funcion enlazada a la propiedad onSubmit de la etiqueta form
+        // console.log( $formSubmit );
+        $formSubmit({ preventDefault(){} });                                //  Simula handleAddTodo y pasamos como argumento la simulacion del event
+
+        expect( handleAddTodo ).toHaveBeenCalledTimes( 1 );                     //  Verifica que la funcion handleAddTodo ha sido invocada al menos una vez
+        expect( handleAddTodo ).toHaveBeenCalledWith( expect.any( Object ) );   //  Verifica que la funcion handleAddTodo ha sido invocada pasandole un objeto 
+        expect( handleAddTodo ).toHaveBeenCalledWith({                          //  Verifica que la funcion handleAddTodo ha sido invocada pasando un objeto con parametros especificos
+            id: expect.any( Number ),       //  Espera cualquier ID de tipo Number
+            description: value,
+            done: false
+        });
+        expect( wrapper.find( 'input' ).prop( 'value' ) ).toBe( '' );           //  Verifica los efectos de la ejecucion de la funcion reset establece el campo del formulario como vacio
+
+    } );
+
 } );
