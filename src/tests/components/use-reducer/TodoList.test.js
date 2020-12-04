465b9e41afb1433e1ef44b092f55f0ae3085a1be
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-12-04 15:55:59 -0500

    [+] TodoList.test: Define 'debe tener todas las propiedades que se pasan a <TodoItem />'

--- src/tests/components/use-reducer/TodoList.test.js
+++ src/tests/components/use-reducer/TodoList.test.js
@@ -34,4 +34,16 @@ describe( '<TodoList />', () => {
 
     } );
 
+    test( 'debe tener todas las propiedades que se pasan a <TodoItem />', () => {
+
+        const $TodoItem = wrapper.find( 'TodoItem' ).at( 0 );       //  Obtiene el primer elemento del TodoItem
+
+        // console.log( wrapper.find( 'TodoItem' ).at( 0 ).props() );
+        // console.log( wrapper.find( 'TodoItem' ).at( 0 ).prop( 'todo' ) );
+        expect( $TodoItem.prop( 'todo' ) ).toEqual( expect.any( Object ) );
+        expect( $TodoItem.prop( 'handleToggle' ) ).toEqual( expect.any( Function ) );
+        expect( $TodoItem.prop( 'handleDelete' ) ).toEqual( expect.any( Function ) );
+
+    } );
+
 } );
